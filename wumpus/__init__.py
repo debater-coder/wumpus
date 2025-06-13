24cbd7461d0f0ed2058677b8835e3bc51d2e73fe
debater-coder <hamzah@syedahmed.net>
2025-06-13 20:00:23 +1000

    refactor: use standard __init__.py for packages and tests

--- wumpus/__init__.py
+++ wumpus/__init__.py
@@ -1,3 +1,5 @@
 from .cave import Cave
 from .level import Level
 from .player import PlayerController
+
+__all__ = ["Cave", "Level", "PlayerController"]
--- wumpus/tests/__main__.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-from .test_hazards import TestWumpus
-from .test_player import TestPlayerController
-
-unittest.main()
