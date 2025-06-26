fcf65fdf204c1c6f6c4f9b2ae6efbdf4b47baee7
debater-coder <hamzah@syedahmed.net>
2025-06-27 09:34:28 +1000

    feat: implement rotations using rotors

--- graphical/scenes/playing/renderer.py
+++ graphical/scenes/playing/renderer.py
@@ -1,9 +1,13 @@
-from kingdon import Algebra
+from kingdon import Algebra, MultiVector
 import numpy as np
 import numpy.typing as npt
+import pygame as pg
 import typing
+import math
 
 from wumpus.level import Level
+
+from graphical.colours import COLOURS
 
 
 class Renderer:
@@ -13,14 +17,93 @@ class Renderer:
     The image plane is parallel to the first two basis vectors (eg: xy)
     with its last coordinate (eg: z) changing to zoom."""
 
-    def __init__(self, level: Level):
+    def __init__(self, level: Level, fov: float = 90):
         self.level = level
-        dimension = len(list(self.level.level.values())[0].coords)
-        self.algebra = Algebra(dimension)
+        self.fov = fov
+        self.dimension = len(list(self.level.level.values())[0].coords)
+        self.algebra = Algebra(self.dimension)  # Clifford algebra
+        self.rotor = self.algebra.scalar(np.array([1]))
 
-        # positions camera at (0, 0, 0, ..., -5)
-        self.camera_pos = self.algebra.vector(np.array([0] * (dimension - 1) + [-5]))
+        self.reset_zoom()
+
+    def reset_zoom(self):
+        """positions camera at (0, 0, 0, ..., -5)"""
+        self.camera_pos = np.array([0] * (self.dimension - 1) + [-5])
+
+    def zoom(self, value: float):
+        """Moves camera on the 'z'-axis by `value`."""
+        self.camera_pos[-1] += value
 
     def perp_dist(self, coord: npt.NDArray) -> float:
-        perpendicular = coord[2:]
-        return typing.cast(float, np.linalg.norm(perpendicular))
+        """
+        Get the perpendicular distance between the image plane and object.
+        In 4D and above there are more than one directions perpendicuar to any
+        2D plane. We define our camera orientation to look down the 'z'-axis
+        (the last coordinate).
+
+        Used for foreshortening.
+
+        Originally, I thought the perpendicular distance should be the
+        length of the component of the vector perpendicular to the image
+        plane. This is not the case, forshortening only applies in the
+        direction parallel to your line of sight. Consider a 1D image
+        line in a 3D space. A single axis is chosen for an image line,
+        but the direction of sight has more than one option
+        (in 3D there are infinitely many vectors perependicuar to a vector).
+        Thus a perpendicuar direction (line of sight) must be chosen.
+        """
+        return (coord - self.camera_pos)[-1]
+
+    def vector_from_multivector(self, mv: MultiVector):
+        """Extracts a numpy array from the vector part of a multivector"""
+        basis_vectors = list(
+            filter(lambda blade: blade[1].grades[0] == 1, list(self.algebra.blades.items()))
+        )  # get grade 1 blades
+
+        return np.array([getattr(mv, basis[0]) for basis in basis_vectors])
+
+    def project(self, coord: npt.NDArray, screen) -> pg.Vector2:
+        """Projects an arbritrary dimension vector onto the image plane."""
+        planar = coord[:2]  # component of cooridnate parallel to image plane
+        return (
+            pg.Vector2(*planar)
+            * screen.get_rect().height
+            / max(
+                1e-3, self.perp_dist(coord) * math.tan(math.radians(self.fov / 2))
+            )  # scale it by perpendicular distance to image plane
+            + pg.Vector2(1920, 1080) / 2  # center on screen
+        )
+
+    def rotated(self, coord: npt.NDArray) -> npt.NDArray:
+        """Get the roated nD coordinates of a coordinate."""
+        return self.vector_from_multivector(self.rotor >> self.algebra.vector(coord))
+
+    def paint(self, surf: pg.surface.Surface):
+        drawn = set()
+
+        for cave in sorted(
+            (caves := self.level.level).values(),
+            key=lambda cave: self.perp_dist(np.array(cave.coords)),
+            reverse=True,
+        ):
+            coords = self.rotated(np.array(cave.coords))
+            for edge in cave.tunnels:
+                edge_coords = self.rotated(np.array(caves[edge].coords))
+                if (cave.location, edge) in drawn:
+                    continue
+                pg.draw.line(
+                    surf,
+                    COLOURS["zinc_50"],
+                    pg.Vector2(self.project(edge_coords, surf)),
+                    pg.Vector2(self.project(coords, surf)),
+                )
+                drawn.add((edge, cave.location))
+
+            pg.draw.circle(
+                surf,
+                pg.Color(COLOURS["amber_900"]).lerp(
+                    (0, 0, 0, 0), max(0, min(0.5, coords[-1]))
+                ),
+                pg.Vector2(self.project(coords, surf)),
+                100 / self.perp_dist(coords),
+            )
--- graphical/scenes/playing/scene.py
+++ graphical/scenes/playing/scene.py
@@ -11,6 +11,7 @@ from graphical.scene import PushScene, Scene, SceneEvent
 from graphical.colours import COLOURS
 
 from graphical.scenes.paused import Paused
+from .renderer import Renderer
 
 
 class Playing(Scene):
@@ -30,31 +31,16 @@ class Playing(Scene):
         )
         self.level = Level(self.map)
 
+        self.renderer = Renderer(self.level, fov=90)
+
     def update(self) -> Iterator[SceneEvent]:
         for event in pg.event.get(eventtype=pg.KEYUP):
             match event.key:
                 case pg.K_ESCAPE:
                     yield PushScene(Paused(self.screen))
 
-    def perp_dist(self, coord: tuple[float, ...]):
-        perpendicular = np.array(coord[2:])
-        perpendicular[-1] += 5  # translate away from camera
-        return math.sqrt(np.dot(perpendicular, perpendicular))
-
-    def project(self, coord: tuple[float, ...]) -> pg.Vector2:
-        """
-        In persepective projection, the size of an object is based on
-        its perependicuar distance to the camera. To project an arbitrary
-        dimension vector we split it into its planar and perpendicular components.
-        """
-        planar = coord[:2]
-        return (
-            pg.Vector2(planar) * self.screen.get_rect().height / self.perp_dist(coord)
-            + pg.Vector2(1920, 1080) / 2
-        )
-
-    def project_radius(self, radius: float, coord: tuple[float, ...]):
-        return radius / self.perp_dist(coord)
+        for event in pg.event.get(eventtype=pg.MOUSEWHEEL):
+            self.renderer.zoom(event.precise_y)
 
     def paint(self):
         self.screen.blit(self.background, (0, 0))
@@ -62,33 +48,7 @@ class Playing(Scene):
             self.text, self.text.get_rect(center=self.screen.get_rect().center)
         )
 
-        drawn = set()
-
-        for cave in sorted(
-            (caves := self.level.level).values(),
-            key=lambda cave: self.perp_dist(cave.coords),
-            reverse=True,
-        ):
-            for edge in cave.tunnels:
-                if (cave.location, edge) in drawn:
-                    continue
-                pg.draw.line(
-                    self.screen,
-                    COLOURS["zinc_50"],
-                    pg.Vector2(self.project(caves[edge].coords)),
-                    pg.Vector2(self.project(cave.coords)),
-                )
-                drawn.add((edge, cave.location))
-
-            print(self.perp_dist(cave.coords))
-            pg.draw.circle(
-                self.screen,
-                pg.Color(COLOURS["amber_900"]).lerp(
-                    (0, 0, 0, 0), max(0, min(0.5, self.perp_dist(cave.coords) / 2 - 2))
-                ),
-                pg.Vector2(self.project(cave.coords)),
-                self.project_radius(100, cave.coords),
-            )
+        self.renderer.paint(self.screen)
 
         pg.display.flip()
 
